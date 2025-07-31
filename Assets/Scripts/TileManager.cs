d4942b91d449a8c2cb6cdba88890d111e3d6b8c8
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2025-07-31 16:51:28 -0700

    Add BPMController and Tilemanager

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    public TileBase inactiveTile;
+    public TileBase activeTile;
+    
+    private Tilemap tilemap;
+    private int[] columnsIdxs;
+    private int currIdx;
+    private int prevIdx;
+
+    private BoundsInt bounds;
+    
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        bounds = tilemap.cellBounds;
+        populateColumns();
+        
+    }
+
+    private void populateColumns()
+    {
+        HashSet<int> columns = new HashSet<int>();
+        foreach (var position in bounds.allPositionsWithin)
+        {
+            if (tilemap.HasTile(position))
+            {
+                columns.Add(position.x);
+            }
+        }
+
+        columnsIdxs = columns.ToArray();
+        currIdx = columnsIdxs[0];
+        prevIdx = columnsIdxs.Last();
+
+    }
+
+    private void OnEnable()
+    {
+        BPMController.OnBeat += HandleBeat;
+    }
+
+    private void OnDisable()
+    {
+        BPMController.OnBeat -= HandleBeat;
+    }
+
+    private void HandleBeat()
+    {
+        // foreach (Vector3Int cellPos in tilemap.cellBounds.allPositionsWithin)
+        // {
+        //     if (!tilemap.HasTile(cellPos)) continue;
+        //     TileBase currTile = tilemap.GetTile(cellPos);
+        //     tilemap.SetTile(cellPos, currTile == tile1 ? tile2 : tile1);
+        // }
+        
+
+        for (int y = bounds.yMin; y < bounds.yMax; y++)
+        {
+            Vector3Int cellPos = new Vector3Int(currIdx, y, 0);
+            Vector3Int prevPos = new Vector3Int(prevIdx, y, 0);
+
+            tilemap.SetTile(prevPos, inactiveTile);
+            tilemap.SetTile(cellPos, activeTile);
+        }
+
+        prevIdx = currIdx;
+        currIdx = (currIdx < (columnsIdxs.Length - 1) / 2) ? currIdx + 1 : columnsIdxs[0];
+    }
+}
